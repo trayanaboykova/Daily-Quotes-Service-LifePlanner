cd86ef6fa9e109199192bcd7bc99006d34b6075a
Trayana Boykova <trayana.boykova@gmail.com>
2025-03-17 18:02:23 +0200

    adding initial resources

---
+++ daily-quotes/src/test/java/app/lifeplanner/dailyquotes/DailyQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package app.lifeplanner.dailyquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
